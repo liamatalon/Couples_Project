7246e9202a02294b5bb703fbf58d0d749067cd3c
ROMI <broromi30@gmail.com>
2025-08-24 14:49:57 +0300

    new

--- screen.py
+++ screen.py
@@ -2,11 +2,15 @@ import pygame
 from pygame.locals import *
 import consts
 
-def create_window():
-        window = pygame.display.set_mode(
+main_window = pygame.display.set_mode(
                 (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+
+mokesh_window = pygame.display.set_mode(
+                (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+
+def create_main_window(window):
         pygame.display.set_caption('Player Movement')
-        window.fill(consts,GREEN)
+        window.fill(consts.GREEN)
         return window
 
 
